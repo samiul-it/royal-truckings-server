ea3f00a6f0d5d0af2e862ab0d1c3248476887067
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-05-07 14:37:39 +0600

    Data Read,Post and Delete Implemented

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require('cors');
+require('dotenv').config();
+const port=process.env.PORT || 5000;
+
+const app=express();
+
+app.use(cors());
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpxgr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+    try{
+        await client.connect();
+        const itemCollection=client.db('royal-truckings').collection('itemListings');
+
+
+        app.get('/items',async(req,res)=>{
+            const query = {};
+            const cursor=itemCollection.find(query);
+            const items= await cursor.toArray();
+            res.send(items);
+        })
+
+        app.get('/item/:id', async (req,res)=>{
+            const id=req.params.id;
+            const query= {_id:ObjectId(id)};
+            const item=await itemCollection.findOne(query);
+            res.send(item);;
+
+        })
+
+        //Posting new Item
+
+        app.post('/item',async(req,res)=>{
+            const newItem=req.body;
+            const result=await itemCollection.insertOne(newItem);
+            res.send(result);
+        })
+
+        //Deleting an Item
+
+        app.delete('/item/:id',async (req,res)=>{
+            const id=req.params;
+            const qurery={_id:ObjectId(id)};
+            const result= await itemCollection.deleteOne(qurery);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+
+};
+
+run().catch(console.dir);
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Server Running!");
+})
+
+app.listen(port,()=>{
+    console.log("Listening Port:",port);
+})
