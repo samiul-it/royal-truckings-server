e028f512286a6ef48553036a1318eb2e7fe5c134
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-05-08 13:08:15 +0600

    JWT Implemented in Server Side

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port=process.env.PORT || 5000;
 
 const app=express();
 
 app.use(cors());
 app.use(express.json())
+
+
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    // console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
+}
 
 
 
@@ -23,6 +43,31 @@ async function run(){
     try{
         await client.connect();
         const itemCollection=client.db('royal-truckings').collection('itemListings');
+
+        //AuTH
+        app.post('/login',async(req,res)=>{
+            const user=req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+                expiresIn:'2d'
+            });
+            res.send(accessToken);
+        })
+
+        //Implementing JWT in My Items
+
+        app.get("/myitems", verifyJWT, async (req, res) => {
+          const authHeader=req.headers.authorization;
+          const decodedEmail = req.decoded.email;
+          const email = req.query.email;
+          if (email === decodedEmail) {
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+          } else {
+            res.status(403).send({ message: "forbidden accessX" });
+          }
+        });
 
 
         app.get('/items',async(req,res)=>{
